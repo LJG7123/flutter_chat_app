bbe784f814961c495c0a9cb60e4c0dcf68c74b9d
ljg7123 <ljg7123@daum.net>
2025-02-26 13:33:39 +0900

    feat: 각 화면 생성 및 라우터 적용

---
+++ lib/presentation/provider/router_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_chat_app/presentation/screen/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => HomeScreen(),
+      ),
+    ],
+  );
+});
---
+++ lib/presentation/screen/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/presentation/screen/chats/chat_list_screen.dart';
+import 'package:flutter_chat_app/presentation/screen/settings/settings_screen.dart';
+import 'package:flutter_chat_app/presentation/screen/users/user_list_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final currentIndexProvider = StateProvider<int>((ref) => 0);
+
+class HomeScreen extends ConsumerWidget {
+  HomeScreen({super.key});
+  
+  final _pages = <Widget>[
+    UserListScreen(),
+    ChatListScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentIndex = ref.watch(currentIndexProvider);
+
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) => ref.read(currentIndexProvider.notifier).state = value,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '유저'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: '채팅'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '설정'),
+        ],
+      ),
+    );
+  }
+}
