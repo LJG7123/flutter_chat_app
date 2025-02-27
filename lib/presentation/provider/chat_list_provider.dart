df7dd21b6ea0a30862a04d553b3297beb960d632
LJG7123 <ljg7123@naver.com>
2025-02-27 09:54:31 +0900

    feat: 채팅 목록 서버 데이터 가져오도록 구현

--- lib/presentation/provider/chat_list_provider.dart
+++ lib/presentation/provider/chat_list_provider.dart
@@ -1,13 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter_chat_app/domain/entity/chat.dart';
+import 'package:flutter_chat_app/domain/usecase/chat_usecase.dart';
+import 'package:flutter_chat_app/presentation/provider/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final chatListProvider = StateNotifierProvider<ChatListNotifier, List<Chat>>(
-    (ref) => ChatListNotifier());
+    (ref) => ChatListNotifier(ref.read(getChatsUseCaseProvider)));
 
 class ChatListNotifier extends StateNotifier<List<Chat>> {
-  ChatListNotifier() : super([
-    Chat(id: '', participants: ['me', '0'], unreadCount: 0, lastMessage: 'hi', lastMessageTime: DateTime(2025,02,25), lastMessageSender: '0'),
-    Chat(id: '', participants: ['me', '1'], unreadCount: 1, lastMessage: 'text', lastMessageTime: DateTime(2025,02,26), lastMessageSender: 'me'),
-    Chat(id: '', participants: ['me', '2'], unreadCount: 2, lastMessage: 'hello', lastMessageTime: DateTime.now(), lastMessageSender: '2'),
-  ]);
+  final GetChatsUseCase getChatsUseCase;
+  StreamSubscription? chatSubscription;
+
+  ChatListNotifier(this.getChatsUseCase)
+      : super([]) {
+    _fetchAllChats();
+  }
+
+  void _fetchAllChats() async {
+    chatSubscription?.cancel();
+    final userId = 'me';
+
+    chatSubscription = getChatsUseCase(userId).listen((event) {
+      state = event;
+    });
+  }
 }
