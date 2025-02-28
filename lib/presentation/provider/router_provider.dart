cf3f155818f21b1e957eb08bc235c30e68efcf65
LJG7123 <ljg7123@naver.com>
2025-02-28 11:16:47 +0900

    feat: router redirection 구현

--- lib/presentation/provider/router_provider.dart
+++ lib/presentation/provider/router_provider.dart
@@ -1,12 +1,28 @@
+import 'package:flutter_chat_app/presentation/provider/auth_provider.dart';
 import 'package:flutter_chat_app/presentation/screen/chats/chat_room_screen.dart';
 import 'package:flutter_chat_app/presentation/screen/home_screen.dart';
 import 'package:flutter_chat_app/presentation/screen/sign_in/sign_in_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+final authLocation = ['/sign_in', '/sign_up'];
+
 final routerProvider = Provider<GoRouter>((ref) {
-  return GoRouter(
-    initialLocation: '/sign_in',
+  final router = GoRouter(
+    initialLocation: '/',
+    redirect: (context, state) {
+      final user = ref.read(authProvider).value;
+
+      if (ref.read(authProvider).isLoading) return null;
+      if (user != null && authLocation.contains(state.matchedLocation)) {
+        return '/';
+      }
+      if (user == null && !authLocation.contains(state.matchedLocation)) {
+        return '/sign_in';
+      }
+
+      return null;
+    },
     routes: [
       GoRoute(
         path: '/',
@@ -31,4 +47,13 @@ final routerProvider = Provider<GoRouter>((ref) {
       ),
     ],
   );
+
+  ref.listen(
+    authProvider.select((async) => async.value),
+    (previous, next) {
+      router.refresh();
+    },
+  );
+
+  return router;
 });
