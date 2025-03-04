921dcbda1116a79b0a490cd621b55d21a73d3eb1
LJG7123 <ljg7123@naver.com>
2025-03-04 11:23:45 +0900

    feat: 현재 로그인한 유저의 채팅 목록을 불러오도록 구현

--- lib/presentation/provider/chat_list_provider.dart
+++ lib/presentation/provider/chat_list_provider.dart
@@ -1,26 +1,32 @@
 import 'dart:async';
 
 import 'package:flutter_chat_app/domain/entity/chat.dart';
+import 'package:flutter_chat_app/domain/entity/user.dart';
 import 'package:flutter_chat_app/domain/usecase/chat_usecase.dart';
+import 'package:flutter_chat_app/presentation/provider/auth_provider.dart';
 import 'package:flutter_chat_app/presentation/provider/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final chatListProvider =
     StateNotifierProvider.autoDispose<ChatListNotifier, List<Chat>>(
-        (ref) => ChatListNotifier(ref.read(getChatsUseCaseProvider)));
+        (ref) => ChatListNotifier(
+              ref.read(authProvider).value,
+              ref.read(getChatsUseCaseProvider),
+            ));
 
 class ChatListNotifier extends StateNotifier<List<Chat>> {
+  final User? currentUser;
   final GetChatsUseCase getChatsUseCase;
   StreamSubscription? chatSubscription;
 
-  ChatListNotifier(this.getChatsUseCase)
-      : super([]) {
+  ChatListNotifier(this.currentUser, this.getChatsUseCase) : super([]) {
     _fetchAllChats();
   }
 
   void _fetchAllChats() async {
     chatSubscription?.cancel();
-    final userId = 'me';
+    final userId = currentUser?.id;
+    if (userId == null) return;
 
     chatSubscription = getChatsUseCase(userId).listen((event) {
       state = event;
