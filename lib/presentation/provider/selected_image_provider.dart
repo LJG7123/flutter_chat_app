0d2288dfebecbf70a69e5ec20df805d3e7aa997c
LJG7123 <ljg7123@naver.com>
2025-03-05 15:44:06 +0900

    feat: 프로필 사진 변경 기능 구현

--- lib/presentation/provider/selected_image_provider.dart
+++ lib/presentation/provider/selected_image_provider.dart
@@ -1,21 +1,26 @@
 import 'dart:io';
 
+import 'package:flutter_chat_app/domain/usecase/image_usecase.dart';
+import 'package:flutter_chat_app/presentation/provider/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 
 final imagePickerProvider = Provider<ImagePicker>((ref) => ImagePicker());
 
 final selectedImageProvider =
-    StateNotifierProvider.autoDispose<SelectedImageNotifier, File?>(
-        (ref) => SelectedImageNotifier(ref.read(imagePickerProvider)));
+    StateNotifierProvider.autoDispose<SelectedImageNotifier, File?>((ref) =>
+        SelectedImageNotifier(ref.read(imagePickerProvider),
+            ref.read(uploadProfileImageUseCase)));
 
 class SelectedImageNotifier extends StateNotifier<File?> {
-  final ImagePicker _picker;
+  final ImagePicker picker;
+  final UploadProfileImageUseCase uploadProfileImageUseCase;
 
-  SelectedImageNotifier(this._picker) : super(null);
+  SelectedImageNotifier(this.picker, this.uploadProfileImageUseCase)
+      : super(null);
 
   Future<void> pickImage(double width) async {
-    final selectedImage = await _picker.pickImage(
+    final selectedImage = await picker.pickImage(
       source: ImageSource.gallery,
       maxWidth: width,
       imageQuality: 80,
@@ -24,5 +29,12 @@ class SelectedImageNotifier extends StateNotifier<File?> {
     if (selectedImage != null) {
       state = File(selectedImage.path);
     }
+  }
+
+  Future<void> uploadProfileImage(String userId) async {
+    final image = state;
+    if (image == null) return;
+
+    return uploadProfileImageUseCase(userId, image);
   }
 }
--- lib/presentation/screen/settings/edit_profile_screen.dart
+++ lib/presentation/screen/settings/edit_profile_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/presentation/provider/auth_provider.dart';
 import 'package:flutter_chat_app/presentation/provider/selected_image_provider.dart';
 import 'package:flutter_chat_app/presentation/widget/expanded_button.dart';
 import 'package:flutter_chat_app/presentation/widget/expanded_progress_button.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class EditProfileScreen extends ConsumerWidget {
   const EditProfileScreen({super.key});
@@ -24,18 +26,49 @@ class EditProfileScreen extends ConsumerWidget {
           Spacer(),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 8),
-            child: ExpandedButton(onPressed: () {
-              ref.read(selectedImageProvider.notifier).pickImage(width);
-            }, text: '이미지 선택'),
+            child: ExpandedButton(
+              onPressed: () {
+                ref.read(selectedImageProvider.notifier).pickImage(width);
+              },
+              text: '이미지 선택',
+            ),
           ),
           SizedBox(height: 8),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 8),
-            child: ExpandedProgressButton(onPressed: () async {}, text: '저장'),
+            child: ExpandedProgressButton(
+              onPressed: () => _saveProfileImage(context, ref),
+              text: '저장',
+            ),
           ),
           Spacer(),
         ],
       ),
     );
+  }
+
+  Future<void> _saveProfileImage(BuildContext context, WidgetRef ref) async {
+    if (ref.read(selectedImageProvider) == null) {
+      _showSnackBar(context, '이미지가 선택되지 않았습니다.');
+      return;
+    }
+
+    final userId = ref.read(authProvider).value?.id;
+    if (userId == null) {
+      _showSnackBar(context, '오류가 발생했습니다.');
+      return;
+    }
+
+    await ref.read(selectedImageProvider.notifier).uploadProfileImage(userId);
+    if (context.mounted) {
+      ref.read(authProvider.notifier).reloadProfileImage();
+      context.pop();
+      _showSnackBar(context, '프로필 사진이 변경되었습니다.');
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 }
